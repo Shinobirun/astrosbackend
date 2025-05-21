88b01c389e284733118e9ba4ad3f989aec38d258
shinobirun <adrian.latorre@gmail.com>
2025-05-21 16:02:33 -0300

    conect render

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const inicializarTurnosBase = require('./scripts/iniciadorTurnosBase.js');
 
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +14,6 @@ app.use(express.json());
 app.use(cors());
 
 // Conexión a la base de datos
-const connectDB = require('./config/db');
-
 connectDB().then(async () => {
   console.log('Conectado a la base de datos');
   
@@ -25,13 +24,13 @@ connectDB().then(async () => {
 // Rutas
 const userRoutes = require('./routes/userRoutes.js');
 const turnoRoutes = require('./routes/turnoRoutes');
-const creditoRoutes= require('./routes/creditoRoutes.js');
-const turnoRouteSema= require('./routes/turnoRouSema.js')
+const creditoRoutes = require('./routes/creditoRoutes.js');
+const turnoRouteSema = require('./routes/turnoRouSema.js');
 
 app.use('/api/users', userRoutes);
 app.use('/api/turnos', turnoRoutes);
 app.use('/api/creditos', creditoRoutes);
-app.use('/api/turnosSemanales',turnoRouteSema)
+app.use('/api/turnosSemanales', turnoRouteSema);
 
 // Iniciar Servidor
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
