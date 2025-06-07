51902c80be59639f66e1035dd099b47304b26edd
shinobirun <adrian.latorre@gmail.com>
2025-06-07 17:30:36 -0300

    jgjgjgjrkie

--- routes/planillaRoutes.js
+++ routes/planillaRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { protect } = require('../middleware/autMiddleware.js');  
-const { asignarUsuarioDesdePlantilla } = require('../controllers/plantMensController.js');
+const { asignarUsuarioDesdePlantilla, obtenerPlantillaPorUsuario, obtenerTurnosSemana } = require('../controllers/plantMensController.js');
 const router = express.Router();
 
 router.post('/asignarDesdePlantilla', protect, asignarUsuarioDesdePlantilla);
+router.get("/:userId", protect, obtenerPlantillaPorUsuario);
+router.get('/turnosSemana', obtenerTurnosSemana);
 
 module.exports = router;
