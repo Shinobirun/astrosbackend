5fbe9d53f4c35caa07f4a217cf34df97e8db92fd
shinobirun <adrian.latorre@gmail.com>
2025-06-22 17:49:38 -0300

    fjfjfj

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 
 // Rutas (antes de iniciar la conexión)
 const userRoutes = require('./routes/userRoutes');
